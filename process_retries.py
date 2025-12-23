8f66bd21c9c4c980c8badf7814666eab110cbd3d
udipth01 <udipth@gmail.com>
2025-12-23 10:56:46 +0530

    corrected code for cron again

--- process_retries.py
+++ process_retries.py
@@ -1,5 +1,5 @@
 # process_retries.py
-from helpers.retry_manager import process_due_retries
+from helpers.retry_manager import   process_call_now_leads,process_due_retries
 import time
 
 if __name__ == "__main__":
@@ -7,7 +7,12 @@ if __name__ == "__main__":
 
     # You can run continuously (recommended)
     while True:
+        
+        print("📞 Checking Call Now leads")
+        process_call_now_leads(limit=50)
+        
         print("⏳ Checking retry queue...")
         results = process_due_retries()
+        
         print("📌 Results:", results)
         time.sleep(60)   # check every 1 minute
